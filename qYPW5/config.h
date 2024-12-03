c0e1338380c9fea20c4cd2e91182b1c1aed0bc6f
Chingkhei Nganba Sapam <sapam@users.noreply.github.com>
2024-12-03 12:24:39 +0530

    Update config.h
    Set DYNAMIC_MACRO_DELAY to 40ms

--- qYPW5/config.h
+++ qYPW5/config.h
@@ -13,4 +13,4 @@
 #define RGB_MATRIX_STARTUP_SPD 60
 
 // Adding Custom Delay of 300 ms between keypressed during Dynamic Macro Replay
-#define DYNAMIC_MACRO_DELAY 80
+#define DYNAMIC_MACRO_DELAY 40
